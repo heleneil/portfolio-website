fcb20ec0f13202c4f1592f9b1ca04e98768d48e2
heleneil <helene.ilvonen@hotmail.com>
2024-08-06 19:26:39 +0300

    added tailwind and css file

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: {
+      relative: true,
+      files: [
+        "./pages/**/*.{html,js}",
+      ],
+    },
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
